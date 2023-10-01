d8ccd396b3b1934bf1728a46e59c5386f7f75314
Alexey Konstantinov <konstantinov.py@gmail.com>
2023-10-01 15:24:30 +0300

    Add simple test

---
+++ test1.py
@@ -0,0 +1,13 @@
+import pytest
+
+
+from app import my_simple_function
+
+
+@pytest.mark.parametrize(['a', 'b'], [[1, 'Halo'], [2, 'babay'], [3, 'Good morning']])
+def test_simple_func(a, b):
+    # a = random.randint(0, 100)
+    # b = 'Hello'
+
+    assert my_simple_function(a, b) == f"{b}-{a}"
+
