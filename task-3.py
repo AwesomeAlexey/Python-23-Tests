11df0dde039dd21d8d70b73bc011923af96ca5d6
Roman_Kulikov <Kulekov.roman@yandex.ru>
2023-10-04 16:06:01 +0300

    added tests for gcd and are_coprime

--- task-3.py
+++ task-3.py
@@ -1,9 +1,8 @@
 import random
 import pytest
-from math import sqrt
+from math import gcd as mgcd
 
-from typing import List, Tuple
-from app import is_prime, next_prime
+from app import is_prime, next_prime, gcd, are_coprime
 
 random.seed(1)
 
@@ -17,7 +16,7 @@ def author_is_prime(number: int) -> bool:
     if number < 2:
         return False
     i = 2
-    while i*i <= number:
+    while i * i <= number:
         if number % i == 0:
             return False
         i += 1
@@ -40,39 +39,16 @@ def author_print_all_primes():
         n += 1
 
 
-
-# def author_get_sequence_length(n: int) -> Tuple[int, List[int]]:
-#     sequence = [n]
-#     while n != 1:
-#         n = 3 * n + 1 if n % 2 else n // 2
-#         sequence.append(n)
-#
-#     return len(sequence), sequence
-
-
-# def author_get_longest_sequence(lower: int, upper: int) -> Tuple[int, int]:
-#
-#     max_length, _ = author_get_sequence_length(lower)
-#     max_n = lower
-#
-#     for n in range(lower + 1, upper + 1):
-#         sequence_length, seq = author_get_sequence_length(n)
-#         if sequence_length > max_length:
-#             max_n = n
-#             max_length = sequence_length
-#
-#     return max_n, max_length
-
-
 def test_is_prime():
     for _ in range(10):
         test_n = random.randint(1, 100)
         solution_answer = is_prime(test_n)
         expected_solution = author_is_prime(test_n)
         assert solution_answer == expected_solution, f"Wrong answer(is_prime): " \
-                                                   f"got {solution_answer}, " \
-                                                   f"but {expected_solution} was expected, " \
-                                                   f"for number = {test_n} "
+                                                     f"got {solution_answer}, " \
+                                                     f"but {expected_solution} was expected, " \
+                                                     f"for number = {test_n} "
+
 
 def test_next_prime():
     for _ in range(10):
@@ -80,29 +56,9 @@ def test_next_prime():
         solution_next = next_prime(test_n)
         expected_next = author_next_prime(test_n)
         assert solution_next == expected_next, f"Wrong next prime(next_prime): " \
-                                                   f"got {solution_next}, " \
-                                                   f"but {expected_next} was expected, " \
-                                                   f"for number = {test_n}"
-
-
-### TODO:
-
-# @pytest.mark.parametrize('n', [19_789_690_303_392_599_159_037, 98_789_690_423_392_599_179_037])
-# def test_hypothesis_is_prime(n):
-#     solution_answer = is_prime(n)
-#     expected_solution = author_is_prime(n)
-#     assert solution_answer == expected_solution, f"Wrong answer(is_prime): " \
-#                                                  f"got {solution_answer}, " \
-#                                                  f"but {expected_solution} was expected, "
-#
-# @pytest.mark.parametrize('n', [19_789_690_303_392_599_159_037, 98_789_690_423_392_599_179_037])
-# def test_hypothesis_next_prime(n):
-#     solution_next = next_prime(n)
-#     expected_next = author_next_prime(n)
-#     assert solution_next == expected_next, f"Wrong answer(is_prime): " \
-#                                                  f"got {solution_next}, " \
-#                                                  f"but {expected_next} was expected, "
-#
+                                               f"got {solution_next}, " \
+                                               f"but {expected_next} was expected, " \
+                                               f"for number = {test_n}"
 
 
 @pytest.mark.parametrize('arg', ['a', '1', '-1', -1, 1.0, 0.5, 0, '1/2', 'ahalay mahalay'])
@@ -116,6 +72,7 @@ def test_bad_args_is_prime(arg):
               f"Bad argument: \"{arg}\", ")
         raise e
 
+
 @pytest.mark.parametrize('arg', ['a', '1', '-1', -1, 1.0, 0.5, 0, '1/2', 'ahalay mahalay'])
 def test_bad_args_next_prime(arg):
     try:
@@ -127,3 +84,54 @@ def test_bad_args_next_prime(arg):
               f"Bad argument: \"{arg}\", ")
         raise e
 
+
+###gcd and are_coprime
+
+def test_gcd():
+    for _ in range(10):
+        num_1 = random.randint(1, 1000)
+        num_2 = random.randint(1, 1000)
+        solution_answer = gcd(num_1, num_2)
+        expected_solution = mgcd(num_1, num_2)
+        assert solution_answer == expected_solution, f"Wrong answer(gcd): " \
+                                                     f"got {solution_answer}, " \
+                                                     f"but {expected_solution} was expected, " \
+                                                     f"for numbers = {num_1}, {num_2} "
+
+
+def test_are_coprime():
+    for _ in range(10):
+        num_1 = random.randint(1, 1000)
+        num_2 = random.randint(1, 1000)
+        solution_answer = are_coprime(num_1, num_2)
+        expected_solution = (mgcd(num_1, num_2) == 1)
+        assert solution_answer == expected_solution, f"Wrong next prime(are_coprime): " \
+                                                     f"got {solution_answer}, " \
+                                                     f"but {expected_solution} was expected, " \
+                                                     f"for numbers = {num_1}, {num_2}"
+
+
+@pytest.mark.parametrize('num_1', ['a', '1', '-1', -1, 1.0, 0.5, 0, '1/2', 'ahalay mahalay', 10])
+@pytest.mark.parametrize('num_2', ['a', '1', '-1', -1, 1.0, 0.5, 0, '1/2', 'ahalay mahalay'])
+def test_bad_args_gcd(num_1, num_2):
+    try:
+        function_result = gcd(num_1, num_2)
+    except AssertionError:
+        pass
+    except Exception as e:
+        print(f"Bad arguments must throw AssertionError."
+              f"Bad arguments: \"{num_1}\", \"{num_2}\" ")
+        raise e
+
+
+@pytest.mark.parametrize('num_1', ['a', '1', '-1', -1, 1.0, 0.5, 0, '1/2', 'ahalay mahalay', 10])
+@pytest.mark.parametrize('num_2', ['a', '1', '-1', -1, 1.0, 0.5, 0, '1/2', 'ahalay mahalay'])
+def test_bad_args_are_coprime(num_1, num_2):
+    try:
+        function_result = are_coprime(num_1, num_2)
+    except AssertionError:
+        pass
+    except Exception as e:
+        print(f"Bad arguments must throw AssertionError."
+              f"Bad arguments: \"{num_1}\", \"{num_2}\" ")
+        raise e
